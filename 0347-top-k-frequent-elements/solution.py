7dc4c6d0d9d3a0adc4781ed079abd6f47bc1117e
David Dávila <daviddavilacorraliza@gmail.com>
2026-08-08 02:03:22 +0000

    [LeetCode Sync] Runtime - 2 ms (92.00%), Memory - 22.6 MB (99.24%)

---
+++ 0347-top-k-frequent-elements/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        count = {}
+
+        for num in nums:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+        
+        sorted_items = sorted(
+            count.items(),
+            key=lambda item: item[1],
+            reverse=True
+        )
+
+        result = []
+        for i in range(k):
+            result.append(sorted_items[i][0])
+
+        return result
